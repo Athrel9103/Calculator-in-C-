00fdcc800d9b946d3efafadcc80f32a67aaa873d
Athrel9103 <relekar.atharv@gmail.com>
2021-10-02 16:35:02 -0400

    initial commit

---
+++ calculator.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "calculator.h"
+
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a, b;
+    char c;
+    cin >> c;
+
+    int result;
+    
+    switch(c){
+    case '+':
+        result = add(a,b);
+        cout << result << endl;
+        break;
+    case '-':
+        result = subtract(a,b);
+        cout << result << endl;
+        break;
+    case '*':
+        result = multiply(a,b);
+        cout << result << endl;
+        break;
+    case '/':
+        result = divide(a,b);
+        cout << result << endl;
+        break;
+    default:
+        cout << "Inputs not valid" << endl;
+    }
+    
+}
